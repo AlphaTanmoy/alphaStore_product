da0a8931c465eccca8ff70cab618bc3ea066d263
AlphaTanmoy <dtanmoy169@gmail.com>
2024-11-22 23:35:05 +0530

    enums controller added

---
+++ src/main/kotlin/com/alphaStore/product/controler/EnumController.kt
@@ -0,0 +1,63 @@
+package com.alphaStore.product.controler
+
+import com.alphaStore.product.enums.DataStatus
+import com.alphaStore.product.enums.DateRangeType
+import com.alphaStore.product.enums.ProductMainCategory
+import com.alphaStore.product.enums.ProductSubCategory
+import com.alphaStore.product.model.PaginationResponse
+import com.alphaStore.product.reqres.EnumOption
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/product")
+class EnumController {
+
+    @GetMapping("/enums")
+    fun getDataStatus(): PaginationResponse<EnumOption> {
+        val toRet: ArrayList<EnumOption> = ArrayList()
+        DataStatus.entries.forEach {
+            toRet.add(
+                EnumOption(
+                    name = it.name,
+                    description = it.nameDescriptor,
+                    category = "DataStatus"
+                )
+            )
+        }
+
+        DateRangeType.entries.forEach {
+            toRet.add(
+                EnumOption(
+                    name = it.name,
+                    description = it.nameDescriptor,
+                    category = "EnumOption"
+                )
+            )
+        }
+
+        ProductMainCategory.entries.forEach {
+            toRet.add(
+                EnumOption(
+                    name = it.name,
+                    description = it.nameDescriptor,
+                    category = "EnumOption"
+                )
+            )
+        }
+
+        ProductSubCategory.entries.forEach {
+            toRet.add(
+                EnumOption(
+                    name = it.name,
+                    description = it.nameDescriptor,
+                    category = "EnumOption"
+                )
+            )
+        }
+
+        return PaginationResponse(data = toRet)
+    }
+
+}
---
+++ src/main/kotlin/com/alphaStore/product/reqres/EnumOption.kt
@@ -0,0 +1,7 @@
+package com.alphaStore.product.reqres
+
+data class EnumOption(
+    var name: String = "",
+    var description: String = "",
+    var category: String = ""
+)

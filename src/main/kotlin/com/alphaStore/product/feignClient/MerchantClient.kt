aeda46881c5c143634a82a9b3739141aa971e671
AlphaTanmoy <tanmoy.das@stableonesoftware.com>
2024-11-27 18:04:52 +0530

    changes

--- src/main/kotlin/com/alphaStore/product/feignClient/MerchantClient.kt
+++ src/main/kotlin/com/alphaStore/product/feignClient/MerchantClient.kt
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 
-@FeignClient(name = "merchant-service", url = "http://localhost:8084/merchant")
+@FeignClient(name = "merchant-service", url = "http://localhost:8085/product")
 interface MerchantClient {
 
     @GetMapping("/{merchantId}")
